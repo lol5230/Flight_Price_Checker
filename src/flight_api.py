8f63204eed22989821221e0c5560892b1d68069f
Luke Leiter <leiterluke@gmail.com>
2023-02-26 18:29:28 -0500

    Added some docstrings

--- src/flight_api.py
+++ src/flight_api.py
@@ -4,6 +4,11 @@ from datetime import datetime
 
 class FlightChecker:
     def __init__(self, api_key):
+        """
+        The FlightChecker class creates an object that interfaces with the FlightData API served by Travelpayouts.
+        To instantiate an object, the token for the API must be passed.
+        :param api_key: String
+        """
         self.api_key = api_key
         self.flight_data = None
 
@@ -17,12 +22,7 @@ class FlightChecker:
         :param return_date: YYYY-MM or YYYY-MM-DD
         :return:The json from the api response
         """
-        # Set the API endpoint and required parameters
-
-        # Build the API endpoint URL with the variables
         url = f"http://api.travelpayouts.com/v1/prices/cheap?origin={origin}&destination={destination}&depart_date={depart_date}&return_date={return_date}&currency=usd&token={self.api_key}"
-
-        # Send the request and retrieve the response in JSON format
         self.flight_data = requests.get(url).json()
 
     def sort_flight_data_by_departure(self):
@@ -31,21 +31,15 @@ class FlightChecker:
         """
         if self.flight_data['data']:
             city = list(self.flight_data["data"].keys())[0]
-
-            # Get the flights dictionary from the data dictionary using the city key
             flights_dict = self.flight_data["data"][city]
 
-            # Create a list of tuples containing the departure date and the flight data
             flights_list = [(datetime.strptime(flights_dict[i]["departure_at"], "%Y-%m-%dT%H:%M:%S%z"), flights_dict[i])
                             for i in flights_dict]
 
-            # Sort the list by the departure date
             sorted_flights = sorted(flights_list, key=lambda x: x[0])
 
-            # Create a new flights dictionary with the sorted data
             sorted_flights_dict = {str(i): sorted_flights[i][1] for i in range(len(sorted_flights))}
 
-            # Create a new JSON object with the sorted data
             sorted_json = {"success": self.flight_data["success"], "data": {"NYC": sorted_flights_dict},
                            "currency": self.flight_data["currency"]}
 
@@ -57,28 +51,20 @@ class FlightChecker:
         """
         Returns the list of flights in order of lowest to highest price
         """
-        # Find the city key in the data dictionary
         if self.flight_data['data']:
             city = list(self.flight_data["data"].keys())[0]
 
-            # Get the flights dictionary from the data dictionary using the city key
             flights_dict = self.flight_data["data"][city]
 
-            # Create a list of tuples containing the price and the flight data
             flights_list = [(flights_dict[i]["price"], flights_dict[i]) for i in flights_dict]
 
-            # Sort the list by the price
             sorted_flights = sorted(flights_list, key=lambda x: x[0])
 
-            # Create a new flights dictionary with the sorted data
             sorted_flights_dict = {str(i): sorted_flights[i][1] for i in range(len(sorted_flights))}
 
-            # Create a new JSON object with the sorted data
             sorted_json = {"success": self.flight_data["success"], "data": {city: sorted_flights_dict},
                            "currency": self.flight_data["currency"]}
 
             return sorted_json
 
         return None
-
-
